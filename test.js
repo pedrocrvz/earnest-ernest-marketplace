09382a10902c54779ffd6acf943fb48ab21f6047
Pedro Cruz <pedrocrvz@gmail.com>
2020-01-31 22:15:21 +0000

    Remove test.js

--- test.js
+++
@@ -1,9 +0,0 @@
-;(async () => {
-    // load libraries
-    require('dotenv').config()
-    const awaitTransactionMined = require('await-transaction-mined')
-    var Web3 = require('web3')
-    this.encodeCall = require('../../test/helpers/encodeCall')
-  
-    var fs = re
-})
